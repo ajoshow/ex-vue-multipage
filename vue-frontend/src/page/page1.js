9ae33bec6de5ef9c4e89e98593198a7ca9c23db2
ajoshow <ajoshow@gmail.com>
2019-04-07 04:31:27 +0800

    fix vue is not defined issue

--- vue-frontend/src/page/page1.js
+++ vue-frontend/src/page/page1.js
@@ -1,4 +1,12 @@
-import '@src/common/libs'
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate);
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios);
 import { required } from 'vuelidate/lib/validators'
 import fetchButtonComponent from '@src/component/fetchButton.vue'
 
